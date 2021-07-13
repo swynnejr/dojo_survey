e2651cb2b5f2e0f11223e2fa1e6b382b9525776e
swynnejr <saucersam@gmail.com>
2021-07-12 20:08:55 -0500

    Not sure what changed

--- server.py
+++ server.py
@@ -2,7 +2,22 @@ from flask import Flask, render_template, request, redirect, session
 app = Flask(__name__)
 app.secret_key = 'this survey has super secret content'
 
+@app.route('/')
+def survey():
 
+    return render_template("index.html")
+
+
+# @app.route('/process')
+# def save_submission():
+
+#     return redirect("/")
+
+
+# @app.route('/result')
+# def confirmation():
+
+#     return render_template("index.html")
 
 
 
